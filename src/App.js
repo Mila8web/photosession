1d07215710bc5b425758e51dbd1870e5463ae729
Mila8web <mila8web@gmail.com>
2021-04-10 17:59:02 +0300

    header block

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import s from './App.modue.scss';
+import s from './App.module.scss';
 import Header from './components/blocks/header/Header';
 
 function App() {
   return (
     <div className={s.App}>
-      <Header/>
+      <Header />
     </div>
   );
 }
--- src/components/blocks/header/Header.js
+++ src/components/blocks/header/Header.js
@@ -1,11 +1,27 @@
 import s from './Header.module.scss';
+import logo from './../../../assets/img/logo.svg';
+import user from './../../../assets/img/user.png';
 
 
 function Header() {
     return (
-        <div className={s.header}>
-                        
-        </div>
+        <header className={s.header}>
+            <div className={s.container}>
+                <div className={s.inner}>
+                    <div className={s.wrap}>
+                        <img src={logo} alt="" className={s.logo} />
+                        <a href="tel:88001231212" className={s.moscowPhone}>Москва</a>
+                        <div className={s.phone}>
+                            <span>Телефон:</span>
+                            <a href="tel:88001231212" className={s.phoneLink}>8(800)-123-12-12</a>
+                        </div>
+                    </div>
+                    <a href="">
+                        <img src={user} alt="" className={s.user} />
+                    </a>
+                </div>
+            </div>
+        </header>
     );
 }
 
